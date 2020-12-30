4bb892cb410f5e43778a84facedf1e5ffb593776
Clientastisch <daniel.lingemann@aol.com>
2020-12-30 20:15:06 +0100

    Rework

---
+++ src/main/java/me/clientastisch/micartey/transformer/annotations/Overwrite.java
@@ -0,0 +1,17 @@
+package me.clientastisch.micartey.transformer.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Overwrite {
+
+    Type value();
+
+    enum Type {
+        BEFORE, AFTER, REPLACE
+    }
+}
