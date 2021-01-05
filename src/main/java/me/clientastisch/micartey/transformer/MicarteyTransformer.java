36efff04f1111e04f9d21e95c0a532f40f5dbc5c
Clientastisch <daniel.lingemann@aol.com>
2021-01-05 17:08:16 +0100

    Add Constructor Inject option

--- src/main/java/me/clientastisch/micartey/transformer/MicarteyTransformer.java
+++ src/main/java/me/clientastisch/micartey/transformer/MicarteyTransformer.java
@@ -2,11 +2,13 @@ package me.clientastisch.micartey.transformer;
 
 import io.vavr.control.Try;
 import javassist.*;
+import lombok.SneakyThrows;
 import me.clientastisch.micartey.transformer.annotations.*;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
 import java.util.Arrays;
@@ -34,31 +36,60 @@ public class MicarteyTransformer implements ClassFileTransformer {
         AtomicReference<byte[]> reference = new AtomicReference<>(classfileBuffer);
 
         for(Class<?> target : this.observed) {
-            if (!target.getAnnotation(Hook.class).value().equals(className.replace("/", ".")))
+            if (!match(className.replace("/", "."), target.getAnnotation(Hook.class).value()))
                 continue;
 
-            CtClass ctClass = Try.ofCallable(() -> this.classPool.get(target.getAnnotation(Hook.class).value())).getOrNull();
+            CtClass ctClass = Try.ofCallable(() -> this.classPool.get(className.replace("/", "."))).getOrNull();
 
-            Arrays.stream(target.getMethods()).filter(method -> method.isAnnotationPresent(Overwrite.class)).forEach(method -> {
-                Overwrite overwrite = method.getAnnotation(Overwrite.class);
-                Field field = target.getAnnotation(Field.class);
+            Try.run(() -> {
+                MicarteyParser<?> parser = new MicarteyParser<>(null, target);
+
+                CtField ctField = CtField.make(parser.buildField(), ctClass);
+                ctClass.addField(ctField);
+            }).onFailure(Throwable::printStackTrace);
+
+            Arrays.stream(target.getConstructors()).filter(constructor -> constructor.isAnnotationPresent(Overwrite.class)).forEach(constructor -> {
+                Overwrite overwrite = constructor.getAnnotation(Overwrite.class);
 
                 Try.run(() -> {
-                    CtField ctField = CtField.make(this.buildFiled(field, target), ctClass);
-                    ctClass.addField(ctField);
+                    MicarteyParser<Constructor<?>> parser = new MicarteyParser<>(constructor, target);
 
-                    CtMethod ctMethod = this.getMethod(ctClass, method);
-                    String methodCall = this.buildInvoke(field, method);
+                    CtConstructor ctConstructor = this.getConstructor(ctClass, constructor);
+                    String instance = parser.buildInstance();
 
                     switch (overwrite.value()) {
                         case BEFORE:
-                            ctMethod.insertBefore(methodCall);
+                            ctConstructor.insertBefore(instance);
                             break;
                         case AFTER:
-                            ctMethod.insertAfter(methodCall);
+                            ctConstructor.insertAfter(instance);
                             break;
                         case REPLACE:
-                            ctMethod.setBody(methodCall);
+                            ctConstructor.setBody(instance);
+                            break;
+                    }
+
+                }).onFailure(Throwable::printStackTrace);
+            });
+
+            Arrays.stream(target.getMethods()).filter(method -> method.isAnnotationPresent(Overwrite.class)).forEach(method -> {
+                Overwrite overwrite = method.getAnnotation(Overwrite.class);
+
+                Try.run(() -> {
+                    MicarteyParser<Method> parser = new MicarteyParser<>(method, target);
+
+                    CtMethod ctMethod = this.getMethod(ctClass, method);
+                    String invoke = parser.buildInvoke();
+
+                    switch (overwrite.value()) {
+                        case BEFORE:
+                            ctMethod.insertBefore(invoke);
+                            break;
+                        case AFTER:
+                            ctMethod.insertAfter(invoke);
+                            break;
+                        case REPLACE:
+                            ctMethod.setBody(invoke);
                             break;
                     }
 
@@ -87,29 +118,24 @@ public class MicarteyTransformer implements ClassFileTransformer {
         }).toArray(CtClass[]::new));
     }
 
-    private String buildInvoke(Field field, Method method) {
-        StringBuilder builder = new StringBuilder("this." + field.value() + "." + method.getName() + "(");
+    private CtConstructor getConstructor(CtClass ctClass, Constructor<?> constructor) throws NotFoundException {
+        Parameter parameter = constructor.getAnnotation(Parameter.class);
 
-        for (int index = 0; index < method.getParameterCount(); index++) {
-            builder.append("$").append(index);
-
-            if (method.getParameterCount() != index + 1)
-                builder.append(",");
-        }
-
-        if (method.isAnnotationPresent(Return.class))
-            builder.insert(0, "return ");
-
-        return builder.append(");").toString();
+        return ctClass.getDeclaredConstructor(Arrays.stream(parameter.value()).map(Class::getName).map(var -> {
+            return Try.ofCallable(() -> this.classPool.get(var)).getOrNull();
+        }).toArray(CtClass[]::new));
     }
 
-    private String buildFiled(Field field, Class<?> target) {
-        return String.format(
-                "private final %s %s = new %s(%s))",
-                target.getName(), field.value(), target.getName(),
-                Arrays.stream(target.getConstructors()).anyMatch(var -> var.getParameterCount() == 1) ? "$1" : ""
-        );
+    private boolean match(String pattern, String parameter) {
+        if (parameter.split("~").length != 2)
+            return pattern.compareTo(parameter) == 0;
+
+        String part = parameter.split("~")[0];
+        String part2 = parameter.split("~")[1];
+
+        return pattern.substring(0, part.length()).compareTo(part) + pattern.substring(pattern.length() - part2.length()).compareTo(part2) == 0;
     }
+
 
     public void retransform(Instrumentation instrumentation) {
         instrumentation.addTransformer(this, true);
