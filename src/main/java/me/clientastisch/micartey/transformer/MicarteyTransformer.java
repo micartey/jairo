00125f21efe954c9eba8798e61115d29597d79a7
Clientastisch <daniel.lingemann@aol.com>
2021-02-12 11:15:07 +0100

    Add Documentation

--- src/main/java/me/clientastisch/micartey/transformer/MicarteyTransformer.java
+++ src/main/java/me/clientastisch/micartey/transformer/MicarteyTransformer.java
@@ -2,7 +2,6 @@ package me.clientastisch.micartey.transformer;
 
 import io.vavr.control.Try;
 import javassist.*;
-import lombok.SneakyThrows;
 import me.clientastisch.micartey.transformer.annotations.*;
 
 import java.lang.instrument.ClassFileTransformer;
@@ -12,7 +11,6 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.security.ProtectionDomain;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -21,12 +19,28 @@ public class MicarteyTransformer implements ClassFileTransformer {
     private final List<Class<?>> observed;
     private final ClassPool classPool;
 
+    /**
+     * Add a list of {@linkplain Class observers} to define rules for
+     * transforming classes.
+     *
+     * Observers need following annotations:
+     * <ul>
+     *     <li>
+     *          {@linkplain Field Field} annotation
+     *     </li>
+     *     <li>
+     *          {@linkplain Hook Hook} annotation
+     *     </li>
+     * </ul>
+     *
+     * @param arguments List of observers
+     */
     public MicarteyTransformer(Class<?>... arguments) {
         if (!Arrays.stream(arguments).allMatch(target -> target.isAnnotationPresent(Hook.class)))
-            throw new IllegalStateException("Some class[es] are missing the annotation: " + Hook.class.getName());
+            throw new IllegalStateException("Some classes are missing the annotation: " + Hook.class.getName());
 
         if (!Arrays.stream(arguments).allMatch(target -> target.isAnnotationPresent(Field.class)))
-            throw new IllegalStateException("Some class[es] are missing the annotation: " + Field.class.getName());
+            throw new IllegalStateException("Some classes are missing the annotation: " + Field.class.getName());
 
         this.observed = Arrays.asList(arguments);
         this.classPool = ClassPool.getDefault();
@@ -105,6 +119,24 @@ public class MicarteyTransformer implements ClassFileTransformer {
         return reference.get();
     }
 
+    /**
+     * Finds a {@linkplain CtMethod CtMethod} according to the pattern presented by
+     * one of the following:
+     *
+     * <ul>
+     *     <li>
+     *         Annotation {@linkplain Parameter @Parameter}
+     *     </li>
+     *     <li>
+     *          Method name
+     *     </li>
+     * </ul>
+     *
+     * @param ctClass Class to rewrite
+     * @param method Method of the observing class
+     * @return {@linkplain CtMethod CtMethod} which is suitable for the {@linkplain Method Method}
+     * @throws NotFoundException Exception is thrown if no {@linkplain CtMethod CtMethod} is found
+     */
     private CtMethod getMethod(CtClass ctClass, Method method) throws NotFoundException {
         Parameter parameter = method.getAnnotation(Parameter.class);
         Name name = method.getAnnotation(Name.class);
@@ -119,27 +151,63 @@ public class MicarteyTransformer implements ClassFileTransformer {
         }).toArray(CtClass[]::new));
     }
 
+    /**
+     * Finds a {@linkplain CtConstructor CtConstructor} according to the pattern presented by
+     * one of the following:
+     *
+     * <ul>
+     *     <li>
+     *         Annotations like {@linkplain Parameter @Parameter}
+     *     </li>
+     *     <li>
+     *          Method {@link java.lang.reflect.Constructor#getTypeParameters signature}
+     *     </li>
+     * </ul>
+     *
+     * @param ctClass Class to rewrite
+     * @param constructor Constructor of the observing class
+     * @return {@linkplain CtConstructor CtConstructor} which is suitable for the {@linkplain Constructor Constructor}
+     * @throws NotFoundException Exception is thrown if no {@linkplain CtConstructor CtConstructor} is found
+     */
     private CtConstructor getConstructor(CtClass ctClass, Constructor<?> constructor) throws NotFoundException {
         Parameter parameter = constructor.getAnnotation(Parameter.class);
+
+        if (!constructor.isAnnotationPresent(Parameter.class))
+            return ctClass.getDeclaredConstructor(Arrays.stream(constructor.getParameterTypes()).map(Class::getName).map(it -> {
+                return Try.ofCallable(() -> this.classPool.get(it)).getOrNull();
+            }).toArray(CtClass[]::new));
 
         return ctClass.getDeclaredConstructor(Arrays.stream(parameter.value()).map(Class::getName).map(var -> {
             return Try.ofCallable(() -> this.classPool.get(var)).getOrNull();
         }).toArray(CtClass[]::new));
     }
 
-    @SuppressWarnings("all")
-    private boolean match(String pattern, String parameter) {
+    /**
+     * Makes sure that both {@linkplain String parameters} matches according to
+     * their pattern
+     *
+     * @param match classname of the class which will be transformed
+     * @param pattern pattern to match the classname
+     * @return Whether pattern matches classname
+     */
+    private boolean match(String match, String pattern) {
+        String backup = match;
 
-        String backup = pattern;
-        for(String string : parameter.split("~")) {
+        for(String string : pattern.split("~")) {
             if (string.length() > 1)
                 backup = backup.replace(string, "");
         }
 
-        return String.format(parameter.replace("~", "%s"), backup.split("\\.")).compareTo(pattern) == 0;
+        return String.format(pattern.replace("~", "%s"), backup.split("\\.")).compareTo(match) == 0;
     }
 
-    @SuppressWarnings("unused")
+    /**
+     * Add {@linkplain MicarteyTransformer MicarteyTransformer} as a new
+     * {@linkplain ClassFileTransformer ClassTransformer} and retransforms already loaded
+     * classes.
+     *
+     * @param instrumentation Instrumentation of Java-agent
+     */
     public void retransform(Instrumentation instrumentation) {
         instrumentation.addTransformer(this, true);
 
