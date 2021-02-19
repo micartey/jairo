7a4073433bdbfd6431bfe5624cb8b04f805e85ec
Clientastisch <daniel.lingemann@aol.com>
2021-02-19 17:31:49 +0100

    Remove Constructor as target

--- src/main/java/me/clientastisch/micartey/transformer/annotations/Overwrite.java
+++ src/main/java/me/clientastisch/micartey/transformer/annotations/Overwrite.java
@@ -29,7 +29,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Overwrite {
 
